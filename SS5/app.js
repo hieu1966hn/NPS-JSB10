59d7cdf7a641628021ad612f7ebec0961492a15a
hieu1966hn <hieu1966hn@gmail.com>
2022-01-23 09:18:51 +0700

    6 kiểu dl cơ bản

--- SS5/app.js
+++ SS5/app.js
@@ -1,1 +1,17 @@
-alert("Hello World!")
+/*
+JS có 6 kiểu dữ liệu cơ bản:
+Number: Dùng cho cả số nguyên và số thực.
+Boolean: lưu trữ hai giá trị đúng (true) và sai (false). Biến kiểu boolean có thể được dùng thay cho điều kiện trong các câu lệnh khác.
+String: String dùng lưu dữ liệu dạng text. Nội dung của string được bao lại trong cặp nháy kép hoặc nháy đơn tùy trường hợp.
+Null: Đơn giản là không có giá trị nào cả.
+Undefined: Là các giá trị không xác định.
+Object: Thể hiện một đối tượng và các thuộc tính có thể truy cập đến.
+
+*/
+
+// VD: Number: 2,3,4,5, -1,-0.12314
+// VD: Boolean: 1<2, 2>1, 1!=0 (so sánh mới có đúng/sai)
+// VD: String: 'Hello', "Hi", `Hello World`
+// VD: null: null
+// VD: undefined: mới khởi tạo biến
+// VD: Đối tượng: tự định nghĩa.
