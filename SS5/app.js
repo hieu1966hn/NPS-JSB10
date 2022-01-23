97d5ee1ca71daae29fca7c31bbbcda0b1b6efd69
hieu1966hn <hieu1966hn@gmail.com>
2022-01-23 09:58:04 +0700

    VD if else

--- SS5/app.js
+++ SS5/app.js
@@ -45,7 +45,7 @@ Toán tử so sánh: <,>,>=,<=,==, ===, !=, !==
 
 
 /*
-Biến trong Javascript là gì? 
+Biến trong Javascript là gì?
 - Dùng để lưu trữ giá trị và sử dụng nó trong JS:
 
 - Cú pháp khai báo biến: let, var, const
@@ -53,43 +53,63 @@ Biến trong Javascript là gì?
 Lưu ý: "=" phép toán gán giá trị;
 */
 
-let a = 1;
-a = 0;// gán lại giá trị của a = 0
-console.log(a);// a = 0
+// let a = 1;
+// a = 0;// gán lại giá trị của a = 0
+// console.log(a);// a = 0
 
-var b = 2;
-b = 1;// gán lại giá trị của b = 1
-console.log(b);
+// var b = 2;
+// b = 1;// gán lại giá trị của b = 1
+// console.log(b);
 
-const Pi = 3.14; // khai bao hang so: Hằng số là giá trị cố định.
+// const Pi = 3.14; // khai bao hang so: Hằng số là giá trị cố định.
 
-console.log(Pi);// Pi = ?
+// console.log(Pi);// Pi = ?
 
-let hello = 'Xin chào tất cả các em'
-console.log(hello);
+// let hello = 'Xin chào tất cả các em'
+// console.log(hello);
 
-let hello2 = "Xin chào mọi người"
-console.log(hello2);
+// let hello2 = "Xin chào mọi người"
+// console.log(hello2);
 
-let hello3 = `Xin chào !!!!!`
-console.log(hello3);
-
-
-// VD về biến đúng/sai
-let dung = 1 < 2; // Boolean
-console.log(dung);
+// let hello3 = `Xin chào !!!!!`
+// console.log(hello3);
 
 
-// VD về null
-let n = null
-console.log(n);
+// // VD về biến đúng/sai
+// let dung = 1 < 2; // Boolean
+// console.log(dung);
 
 
-let k;
-console.log(k); //  undefined
+// // VD về null
+// let n = null
+// console.log(n);
 
-////// Lưu ý: Hằng số không thay đổi được, Biến có thể lưu mọi giá trị và kiểu giữ liệu.
 
+// let k;
+// console.log(k); //  undefined
+
+// //// Lưu ý: Hằng số không thay đổi được, Biến có thể lưu mọi giá trị và kiểu giữ liệu.
+
+
+/*
+Nếu thì (if - else) trong lập trình? 
+
+- Nếu điểm < 5 thì học sinh trung bình.
+- Nếu điểm == 7 thì học sinh đạt danh hiệu khá
+*/
+let x = 7;
+if (x < 5) {
+  // Nếu điều kiện đúng thì chạy code ở đây
+  console.log("Bạn đạt học sinh trung bình");
+}
+else if (x == 7) {
+  // Điều kiện con
+  console.log("Bạn đạt học sinh khá");
+}
+else {
+  // Nếu điều kiện sai thì chạy code ở đây
+  console.log("Bạn không phải học sinh trung bình");
+}
 
 
 
