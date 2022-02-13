72b5487e596904fb2783da0821cdb07e62e3e6c8
hieu1966hn <hieu1966hn@gmail.com>
2022-02-13 09:18:26 +0700

    done for

--- SS6/app.js
+++ SS6/app.js
@@ -22,10 +22,38 @@ B2: Kiểm tra điều kiện
 B3: Nếu điều kiện đúng -> Chạy code trong "do" -> về B2
 B4: Nếu điều kiện sai -> thoát khỏi vòng lặp
 */
-let i = 1;
-do {
-  // coding
-  console.log(i); // 1
-  i = i + 1; // 2
+// let i = 1;
+// do {
+//   // coding
+//   console.log(i); // 1
+//   i = i + 1; // 2
+// }
+// while (i <= 10);
+
+/* Cho người dùng nhập vào số bất kỳ: với đk như sau
+1. Nếu là số dương -> in ra nó
+2. Nếu là số âm -> bắt người dùng nhập lại
+*/
+
+//// for:
+// C1:
+// let i = 1;
+// for (; i <= 10;) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+
+// C2:
+/*
+B1: Chạy câu lệnh khởi tạo đầu tiên ở ô 1
+B2: Kiểm tra điều kiện
+B3: Nếu điều kiện đúng -> chạy code trong ngoặc {} -> chạy câu lệnh ở ô 3 -> Quay về B2
+B4: Nếu điều kiện sai -> Thoát vòng lặp
+
+
+*/
+
+for (let i = 1; i <= 10; i = i + 1) {
+  console.log(i);
 }
-while (i <= 10);
