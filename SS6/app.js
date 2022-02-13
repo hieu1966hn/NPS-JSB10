bf8311fbfc311bc49630f0526b2e4575ee368a94
hieu1966hn <hieu1966hn@gmail.com>
2022-02-13 08:54:32 +0700

    done while

--- SS6/app.js
+++ SS6/app.js
@@ -1,1 +1,15 @@
 // Tiếp tục học về Javascript tại đây nhé
+
+/////// 3 vòng lặp cơ bản
+
+//// While: In ra các số từ 1 -> 10
+/*
+B1: Kiểm tra điều kiện (điều kiện dừng)
+B2: Nếu điều kiện đúng -> CHạy code bên trong while -> quay lại bước 1
+B3: Nếu điều kiện sai -> Thoát khỏi vòng lặp.
+*/
+let i = 1;
+while (i <= 10) {
+  console.log(i); // 1, 2,... 10
+  i = i + 1; // 2, 3, ..., 11
+}
