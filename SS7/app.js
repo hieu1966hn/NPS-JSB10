4dd50841e55c8a0a48dcaf7dc3e0614524735987
hieu1966hn <hieu1966hn@gmail.com>
2022-02-20 10:19:43 +0700

    done Ứng dụng C,R,U,D

--- SS7/app.js
+++ SS7/app.js
@@ -54,19 +54,74 @@
 
 
 
-//// Code Mới SS7
-let arr = [];
-////// C,R,U,D
-/// C: Create: thêm phần tử vào trong mảng
-arr.push('hi', "Hello", "How are you")
+// //// Code Mới SS7
+// let arr = [];
+// ////// C,R,U,D
+// /// C: Create: thêm phần tử vào trong mảng
+// arr.push('hi', "Hello", "How are you")
 
 
-///// R: Read: lấy ra danh sách phần tử trong mảng
-console.log(arr.join(", "));
-// join: Dùng để tách các phần tử trong mảng thành chuỗi
+// ///// R: Read: lấy ra danh sách phần tử trong mảng
+// console.log(arr.join(", "));
+// // join: Dùng để tách các phần tử trong mảng thành chuỗi
 
-// U: Update phần tử trong mảng: (phải biết vị trí phần tử đó ở đâu)
-arr[2] = "am fine thank you, and you"
+// // U: Update phần tử trong mảng: (phải biết vị trí phần tử đó ở đâu)
+// arr[2] = "am fine thank you, and you"
 
-// D: Delete phần tử trong mảng: (phải biết vị trí phần tử đó ở đâu)
-arr.splice(1, 1)
+// // D: Delete phần tử trong mảng: (phải biết vị trí phần tử đó ở đâu)
+// arr.splice(1, 1)
+
+
+/*
+Bài tập với nhà hàng: 
+Một nhà hàng mới xây dựng có menu hiện tại là 
+menu = ['thit cho', 'rau xao', 'rau luoc']
+
+Y/C: xây dựng trương trình như sau:
+Mô tả: Khi người vào Web sẽ hiển thị ra 4 lựa chọn là C,R,U,D cho người quản trị. Nếu người admin nhập sai
+Nếu nhập sai thì nhập lại.
+1. Chọn C => Cho admin nhập vào một món ăn 
+2. Chọn R => In ra cho admin danh sách món ăn hiện tại
+3. Chọn U => Cho admin nhập vào ten mon muon update => nhap ten mon moi vao
+4. Chọn D => Cho admin nhập vào tên món muốn delete => Xóa món đó đi
+*/
+
+
+let menu = ['thit cho', 'rau xao', 'rau luoc'];
+let input;
+do {
+  input = prompt("Người dùng nhập vào c,r,u,d").trim().toLowerCase();
+  if (input == "c") {
+    let temp = prompt("Mời admin nhập món ăn muốn thêm");
+    menu.push(temp)
+  }
+  else if (input == "r") {
+    console.log("Menu hiện tại là: " + menu.join(", "));
+  }
+  else if (input == "u") {
+    let monMuonCapNhat = prompt("Mời admin nhập món muốn sửa").trim().toLowerCase();
+    if (menu.indexOf(monMuonCapNhat) >= 0) {
+      menu[menu.indexOf(monMuonCapNhat)] = prompt("Mời admin nhập món mới").trim().toLowerCase();
+    }
+    else {
+      alert("Không tồn tại món này")
+    }
+  }
+  else if (input == 'd') {
+    let monMuonXoa = prompt("Mời admin nhập món muốn xóa").trim().toLowerCase();
+    if (menu.indexOf(monMuonXoa) >= 0) {
+      menu.splice(menu.indexOf(monMuonXoa), 1)
+    }
+    else {
+      alert("Không tồn tại món này")
+    }
+  }
+  else {
+    alert("Không tồn tại phương thức đã chọn, mời nhập lại")
+  }
+
+  input = "";
+
+}
+while (input != 'c' && input != 'r' && input != 'u' && input != 'd');
+
