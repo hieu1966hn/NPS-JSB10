ddf5c2dc95aad9c03e2d6158a53ef4bdc8acbb1b
hieu1966hn <hieu1966hn@gmail.com>
2022-02-20 09:02:35 +0700

    done C3

--- SS7/app.js
+++ SS7/app.js
@@ -13,7 +13,13 @@ let fruits = [
 // }
 
 // C2: Vòng lặp rút gọn for in
-for (let index in fruits) {
-  // console.log(index);// in ra gì vị trí của từng phần tử mảng
-  console.log(`Tên quả ${(fruits[index])[0]} với số lượng:${(fruits[index])[1]}`);
+// for (let index in fruits) {
+//   // console.log(index);// in ra gì vị trí của từng phần tử mảng
+//   console.log(`Tên quả ${(fruits[index])[0]} với số lượng:${(fruits[index])[1]}`);
+// }
+
+// C3: Vòng lặp rút gọn for of
+for (let key of fruits) {
+  // console.log(key); // in ra gì? In ra giá trị của từng phần tử trong mảng
+  console.log(`Tên quả ${key[0]} với số lượng:${key[1]}`);
 }
