cd98541c97a1988657a5af3732b7a69ee7455270
hieu1966hn <hieu1966hn@gmail.com>
2022-02-20 08:56:18 +0700

    Done in ra

---
+++ SS7/app.js
@@ -0,0 +1,14 @@
+// Chữa bài 1 lab
+let fruits = [
+  ["grape", 5],
+  ["banana", 10],
+  ["watermelon", 3],
+  ["apple", 15],
+];
+
+// C1: Vòng lặp for
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`Tên quả ${(fruits[i])[0]} với số lượng:${(fruits[i])[1]}   `);
+  // truyền biến vào chuỗi: ``
+
+}
